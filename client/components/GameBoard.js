307dea40686343a501ae75c7f3655483c34d5165
Jared Grippe <jared@deadlyicon.com>
2023-01-17 19:30:41 -0800

    WIP

--- client/components/GameBoard.js
+++ client/components/GameBoard.js
@@ -2,8 +2,26 @@ import React from 'react'
 import sortBy from 'lodash/sortBy'
 import { usePlayers } from '../players.js'
 
+const SIZE = 10
 export default function GameBoard(){
-  return <div>
+  return <div style={{
+    width: '90vw',
+    height: '90vw',
+    display: 'grid',
+    gridTemplateColumns: `repeat(${SIZE}, 1fr)`,
+  }}>
+    {Array(10).fill().map((_, x) =>
+      <div key={x} style={{}}>
+        {Array(10).fill().map((_, y) =>
+          <div key={y} style={{
+            height: '10%',
+            width: '10%',
+          }}>
+            .
+          </div>
+        )}
+      </div>
+    )}
     <Players/>
   </div>
 }
